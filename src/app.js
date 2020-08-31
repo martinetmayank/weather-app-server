57ef6b9014d038d413d0a476327d61dc03e6ccf1
Mayank <thedeveloper.mayank@gmail.com>
2020-09-01 01:41:22 +0530

    Added express handlebars 'hbs', dynamic webpage

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+
+// ? Adding template engine 'express handlebars: hbs'
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+
+// ? We are using hbs rendering...
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Mayank'
+    })
+})
+
+
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Mayank'
+    })
+})
+
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Mayank',
+        helpText: 'This is my help'
+    })
+})
 
 // ** Weather app
 app.get('/weather', (req, res) => {
