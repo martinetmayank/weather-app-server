7312fbc17028f35c7fcc9c799d71aff2f8a3bb2d
Mayank <thedeveloper.mayank@gmail.com>
2020-09-02 01:59:27 +0530

    added partials, header, footer, hbs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 
 const app = express()
+
+// Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../template/views')
+const partialsPath = path.join(__dirname, '../template/partials')
 
-
-// ? Adding template engine 'express handlebars: hbs'
+// Adding template engine 'express handlebars: hbs'
 app.set('view engine', 'hbs')
+
+// setup views location
+app.set('views', viewsPath)
+
+// Partials
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to work
 app.use(express.static(publicDirPath))
 
 
-// ? We are using hbs rendering...
+//  ** ----------------------------------------------------------------------------
+//  ** ----------------------------------------------------------------------------
+// We are using hbs rendering...
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
@@ -19,8 +33,7 @@ app.get('', (req, res) => {
     })
 })
 
-
-
+// About page
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
@@ -28,7 +41,7 @@ app.get('/about', (req, res) => {
     })
 })
 
-
+// Help page
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
@@ -37,7 +50,7 @@ app.get('/help', (req, res) => {
     })
 })
 
-// ** Weather app
+// Weather app page
 app.get('/weather', (req, res) => {
     res.send({
         forecast: '20.0',
@@ -45,7 +58,7 @@ app.get('/weather', (req, res) => {
     })
 })
 
-
+//  ** ----------------------------------------------------------------------------
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000...')
