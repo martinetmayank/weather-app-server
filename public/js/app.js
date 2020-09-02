4c43611feb7e859e0e05b372735dc6aceafd7d12
Mayank <thedeveloper.mayank@gmail.com>
2020-09-03 01:31:46 +0530

    used fetch API, created form, logging to console

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 console.log('Javascript file loaded...')
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = searchElement.value
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+
+})
--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ app.get('/weather', (req, res) => {
         latitude,
         longitude,
         location
-    }) => {
+    } = {}) => {
         if (error) {
             return res.send({
                 error
